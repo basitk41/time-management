934190e44f3f230c84d7e1fc95f6b05a23762782
rafi ullah <rafiullah372@gmail.com>
2021-10-16 00:51:19 +0500

    Task#2-Login page UI

---
+++ client/src/Components/Login/Login.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import styles from './Login.module.css'
+
+
+const Login = () => {
+    return (
+        <div className={styles.loginWrap}>
+            <div className={styles.formWrap}>
+                <h3>Login</h3>
+                <div className="form-group mb-3">
+                    <input type="text"
+                    className={`form-control ${styles.formControl}`}
+                    placeholder="Enter Email"
+                    />
+                </div>
+                <div className="form-group mb-3">
+                    <input type="password"
+                    className={`form-control ${styles.formControl}`}
+                    placeholder="Enter Password"
+                    />
+                </div>
+                <button className={styles.loginBtn}>
+                    Login
+                </button>
+                <p>If don't have account 
+                <span>
+                SignUp?
+                </span>
+                 </p>
+            </div>
+        </div>
+    )
+}
+
+export default Login
