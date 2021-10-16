567e01b9cf5165c612b721dc389760de9d6ed62f
Basit Ali <basitk41@gmail.com>
2021-10-16 23:25:13 +0500

    auth route added

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 const data = require("./routes/data");
+const auth = require("./routes/auth");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/v1/data", data);
+app.use("/api/v1/auth", auth);
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
