7b9f27aa0eea190c36b9ea8756bb2db54a66386f
Bilal <notgivingfirst@gmail.com>
2021-10-25 21:08:16 +0500

    hooked up register API

--- client/src/Components/Register/Register.js
+++ client/src/Components/Register/Register.js
@@ -1,11 +1,24 @@
 import React, { useState } from "react";
 import styles from "./Register.module.css";
 import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { register } from "../../Store/Actions";
 const Register = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
+  const [valid, setValid] = useState(false);
+  const dispatch = useDispatch();
+  const handleClick = () => {
+    if (name && email && password) {
+      setValid(false);
+      setName("");
+      setEmail("");
+      setPassword("");
+    } else {
+      setValid(true);
+    }
+  };
   return (
     <div className={styles.Register}>
       <div className={styles.registerWrap}>
@@ -18,6 +31,11 @@ const Register = () => {
             value={name}
             onChange={(e) => setName(e.target.value)}
           />
+          {valid && !name ? (
+            <span className={styles.errorMessage}>
+              Please enter your name *
+            </span>
+          ) : null}
         </div>
         <div className="form-group mb-3">
           <input
@@ -27,17 +45,33 @@ const Register = () => {
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
+          {valid && !email ? (
+            <span className={styles.errorMessage}>
+              Please enter valid email *
+            </span>
+          ) : null}
         </div>
         <div className="form-group mb-3">
           <input
-            type="text"
+            type="password"
             className={`form-control ${styles.formControl}`}
             placeholder="Password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
+          {valid && !password ? (
+            <span className={styles.errorMessage}>Please enter password *</span>
+          ) : null}
         </div>
-        <button className={styles.btn}>Sign up</button>
+        <button
+          className={styles.btn}
+          onClick={() => {
+            dispatch(register({ name, email, password }));
+            handleClick();
+          }}
+        >
+          Sign up
+        </button>
         <p>
           If already have account
           <Link to="/login">Login</Link>
